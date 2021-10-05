27f04d2bf723a4b18c56f436b635071778ac9f87
Marigameo <mariappangameo@gmail.com>
2021-10-05 20:05:21 +0530

    Adding SEO related meta tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
+<svelte:head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pixel VG - WebGL Pixel Editor</title>
+	<meta name="description" content="Pixel (V)ector (G)raphics Easy to use point & click pixel-art editor.">
+</svelte:head>
+
 <script lang="ts">
 	import Controls from '$lib/controls.svelte';
 	import DrawingBoard from '$lib/drawing-board.svelte';
