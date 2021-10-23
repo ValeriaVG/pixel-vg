c2bb17260ba80d05efb0991886fa1943108141db
Valeria Viana Gusmao <valeria.viana.gusmao@gmail.com>
2021-10-23 19:10:06 +0200

    fix: Draw grid properly

--- src/lib/drawing-board.svelte
+++ src/lib/drawing-board.svelte
@@ -147,8 +147,10 @@
 
                 void main() {
                     if(
-                        mod(gl_FragCoord.x,size)<1.0 ||
-                        mod(gl_FragCoord.y,size)<1.0
+						gl_FragCoord.x<1.0||
+						gl_FragCoord.y<1.0||
+						mod(gl_FragCoord.x,size)>(size-1.0) ||
+						mod(gl_FragCoord.y,size)>(size-1.0)
                     ){
                         gl_FragColor = vec4(0.0, 0.0, 0.0, 0.8);
                     }else {discard;}    
